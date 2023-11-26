422f934ab00922e6ad637c2d4edb82bbbb525532
aminedev24 <abdoufuntuber@gmail.com>
2023-11-26 16:26:25 +0100

    first commit
    first commit

--- main.js
+++ main.js
@@ -61,6 +61,7 @@ if (!fs.existsSync(dataDirectoryPath)) {
 
 const MaterialsDataFilePath = path.join(dataDirectoryPath, 'materialsData.json');
 const ProductsDataFilePath = path.join(dataDirectoryPath, 'productsData.json');
+const transactionsFilePath = path.join(dataDirectoryPath, 'transactions.json');
 
 // Create productsData.json if it doesn't exist
 if (!fs.existsSync(ProductsDataFilePath)) {
@@ -73,6 +74,15 @@ if (!fs.existsSync(MaterialsDataFilePath)) {
   // Create an empty array if the file doesn't exist
   fs.writeFileSync(MaterialsDataFilePath, '[]');
 }
+
+
+// Create transactions.json if it doesn't exist
+if (!fs.existsSync(transactionsFilePath)) {
+  // Create an empty array if the file doesn't exist
+  fs.writeFileSync(transactionsFilePath, '{}');
+}
+
+
 
 console.log(userDataPath)
 // Function to read data from a JSON file
@@ -100,6 +110,12 @@ async function writeDataToJsonFile(filePath, data) {
   }
 }
 
+// Handle IPC event to save transactions
+ipcMain.on('saveTransactions', (event, transactions) => {
+  saveTransactionsToFile(transactions);
+  //console.log(transactions)
+  event.sender.send('transactionsSaved', 'Transactions saved successfully');
+});
 // ...
 
 ipcMain.on('getDataFromDirectory', async (event) => {
@@ -107,6 +123,7 @@ ipcMain.on('getDataFromDirectory', async (event) => {
   try {
     const materialsJsonData = await readDataFromJsonFile(MaterialsDataFilePath) || [];
     const productsJsonData = await readDataFromJsonFile(ProductsDataFilePath) || [];
+    const transactionsJsonData = await readDataFromJsonFile(transactionsFilePath) || {};
 
     //console.log(productsJsonData)
 
@@ -115,6 +132,7 @@ ipcMain.on('getDataFromDirectory', async (event) => {
       data: {
         materialsData: materialsJsonData,
         productsData: productsJsonData,
+        transactionsData: transactionsJsonData
       },
     });
   } catch (error) {
@@ -123,32 +141,38 @@ ipcMain.on('getDataFromDirectory', async (event) => {
 });
 
 
-
-
-// ...
-
 ipcMain.on('saveData', async (event, data) => {
   const MaterialsDataFilePath = path.join(dataDirectoryPath, 'materialsData.json');
   const ProductsDataFilePath = path.join(dataDirectoryPath, 'productsData.json');
+  const transactionsFilePath = path.join(dataDirectoryPath, 'transactions.json');
 
   try {
-    if (data.type === 'material') {
-      if (await writeDataToJsonFile(MaterialsDataFilePath, data)) {
+    if (data.type === 'material' || data.type === 'product') {
+      // Handle saving materials and products data
+      const filePath = data.type === 'material' ? MaterialsDataFilePath : ProductsDataFilePath;
+      if (await writeDataToJsonFile(filePath, data)) {
         event.reply('saveDataResponse', { success: true });
       } else {
         event.reply('saveDataResponse', { success: false, error: 'Failed to save data' });
       }
-    } else if (data.type === 'product') {
-      if (await writeDataToJsonFile(ProductsDataFilePath, data)) {
+    } else if (data.type === 'transaction') {
+      // Handle saving transactions separately
+      const existingTransactions = await readDataFromJsonFile(transactionsFilePath) || {};
+      const newTransactions = { ...existingTransactions, ...data };
+
+      if (await writeDataToJsonFile(transactionsFilePath, newTransactions)) {
         event.reply('saveDataResponse', { success: true });
       } else {
         event.reply('saveDataResponse', { success: false, error: 'Failed to save data' });
       }
+    } else {
+      event.reply('saveDataResponse', { success: false, error: 'Unknown data type' });
     }
   } catch (error) {
     event.reply('saveDataResponse', { success: false, error: error.message });
   }
 });
+
 
 // ...
 
@@ -243,6 +267,42 @@ ipcMain.on('updateMaterialQuantities', (event, updatedMaterialsData) => {
     event.reply('updateMaterialQuantitiesResponse', { success: false, error: 'Failed to update material quantities' });
   }
 });
+
+ipcMain.on('updateQuantities', (event, payload) => {
+  const { data, type } = payload;
+
+  // Handle the update based on the type (e.g., material, transaction, etc.)
+  if (type === 'material') {
+      const MaterialsDataFilePath = path.join(dataDirectoryPath, 'materialsData.json');
+      //console.log(payload)
+      // Update the material data in your data source (e.g., JSON file)
+      try {
+        fs.writeFileSync(MaterialsDataFilePath, JSON.stringify(data));
+        console.log('Material quantities updated and saved successfully.');
+        event.reply('updateMaterialQuantitiesResponse', { success: true });
+      } catch (err) {
+        console.error('Error updating material quantities:', err);
+        event.reply('updateMaterialQuantitiesResponse', { success: false, error: 'Failed to update material quantities' });
+      }
+  } 
+  else if (type === 'transaction') {
+    // Handle transaction update
+    //console.log(data)
+     const ProductsDataFilePath = path.join(dataDirectoryPath, 'productsData.json');
+     //console.log(data);
+      try {
+        fs.writeFileSync(ProductsDataFilePath, JSON.stringify(data));
+        console.log('Product quantities updated and saved successfully.');
+        event.reply('updateProductsQuantitiesResponse', { success: true });
+      } catch (err) {
+        console.error('Error updating product quantities:', err);
+        event.reply('updateProductQuantitiesResponse', { success: false, error: 'Failed to update product quantities' });
+      }
+       
+}
+  // Handle other data types if needed
+});
+
 
 // ...
 
